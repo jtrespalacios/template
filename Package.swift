03b2248cc12c86992b8e0535e2f2cb0488117f74
Jeff Trespalacios <jeff.trespalacios@gmail.com>
2018-07-21 00:08:55 -0700

    Updates

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ let package = Package(
     name: "VaporApp",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.3"),
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/redis.git", from: "3.0.0-rc.2.6"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc.3"),
         .package(url: "https://github.com/vapor/url-encoded-form.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2"),
         .package(url: "https://github.com/brokenhandsio/VaporSecurityHeaders.git", from: "2.0.0"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0-rc.2"),
         .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.6.1"),
         .package(url: "https://github.com/nodes-vapor/flash.git", from: "3.0.0")
     ],
@@ -23,7 +23,10 @@ let package = Package(
             "Redis",
             "Authentication",
             "URLEncodedForm",
-            "VaporSecurityHeaders"
+            "VaporSecurityHeaders",
+            "Flash",
+            "JWT",
+            "JWTMiddleware"
             ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
